c00fae8214a36632257c265ae5e480e97667b815
Stone <jinaiwangli521@163.com>
2017-02-04 10:38:39 +0800

    初始化项目

---
+++ DouYuTV/DouYuTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DouYuTV
+//
+//  Created by emiyajin on 17/2/4.
+//  Copyright © 2017年 emiyajin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
